ef7f96d534a1ca561bd4937a8cc3806eb27fd16f
DoncusSpelunkus <marcuspetersen@live.dk>
2023-03-14 14:13:58 +0100

    New commit

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,26 +1,27 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import  'package:intl/intl.dart';
 
 class FireBaseDB{
-  DatabaseReference ref = FirebaseDatabase.instance.ref("Data/Client1/new");
 
+  DatabaseReference ref = FirebaseDatabase.instance.ref("");
 
   initDB() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-
     print("Get's called");
   }
 
-  testSet() async{
+  testSet(double weight) async{
+    setRef();
     await ref.set({
-      "name": "John",
-      "age": 18,
-      "address": {
-        "line1": "100 Mountain View"
-      }
+      "Weight": weight.toString(),
     });
+  }
+
+  setRef(){
+    ref = FirebaseDatabase.instance.ref("Weight/${DateFormat("yyyy-MM-dd").format(DateTime.now())}/${DateFormat("HH:mm:ss").format(DateTime.now())}");
   }
 }
--- lib/ui.dart
+++ lib/ui.dart
@@ -1,9 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:fooddataagg/firebase.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +19,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -28,52 +28,67 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   FireBaseDB db = FireBaseDB();
+  double _currentSliderValue = 20;
 
-  void onButtonPress(){
-    db.testSet();
-  }
-
-  Widget buildButton(String buttonText) {
-    return Expanded(
-      child: SizedBox(
-        height: 70.0,
-        child: Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: OutlinedButton(
-            child: Text(
-              buttonText,
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            onPressed: () => (onButtonPress()),
-          ),
-        ),
-      ),
-    );
+  void circleButtonPress(double weight) {
+    db.testSet(weight);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column( children: [
-        Container(
-          child: Row(
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "Statistics"),
+                Tab(text: "Settings"),
+                Tab(text: "Control"),
+              ],
+            ),
+            title: const Text('Tabs Demo'),
+          ),
+          body: TabBarView(
             children: [
-              buildButton("Statistics"),
-              buildButton("Settings"),
-              buildButton("Control")
-             ],
-            )
-          )
-        ]
+              Text("Stats placeholder"),
+              Icon(Icons.directions_transit),
+              Column(children: [
+                Padding(
+                    padding: EdgeInsets.only(top: 200.0, bottom: 50.0),
+                    child: Expanded(
+                      child: ElevatedButton(
+                        onPressed: () => (circleButtonPress(_currentSliderValue)),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(75),
+                          backgroundColor: Colors.blue, // <-- Button color
+                          foregroundColor: Colors.white, // <-- Splash color),)
+                        ),
+                        child: const Text("Feed now"),
+                      ),
+                    )),
+                Column(children: [
+                  const Text("Choose amount"),
+                  Slider(
+                    value: _currentSliderValue,
+                    max: 200,
+                    divisions: 40,
+                    label: "${_currentSliderValue.round()}g",
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValue = value;
+                      });
+                    },
+                  ),
+                ]),
+              ]),
+            ],
+          ),
+        ),
       ),
     );
   }
